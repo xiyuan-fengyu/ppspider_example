527188df283dacebd6b317cfd41609ccb8947413
xiyuan_fengyu <xiyuan_fengyu@163.com>
2018-06-12 18:09:20 +0800

    文档对应的例子更新

---
+++ src/debug/App.ts
@@ -0,0 +1,18 @@
+import {Launcher, PuppeteerWorkerFactory} from "ppspider";
+import {TestTask} from "./tasks/TestTask";
+
+@Launcher({
+    workplace: __dirname + "/workplace",
+    tasks: [
+        TestTask
+    ],
+    workerFactorys: [
+        new PuppeteerWorkerFactory({
+            headless: false,
+            devtools: true
+        })
+    ]
+})
+class App {
+
+}
---
+++ src/debug/tasks/TestTask.ts
@@ -0,0 +1,21 @@
+import {Job, OnStart, PuppeteerWorkerFactory} from "ppspider";
+import {Page} from "puppeteer";
+
+export class TestTask {
+
+    @OnStart({
+        urls: "http://www.baidu.com",
+        workerFactory: PuppeteerWorkerFactory
+    })
+    async index(page: Page, job: Job) {
+        await page.goto(job.url());
+        const title = await page.evaluate(() => {
+           debugger;
+           const title = document.title;
+           console.log(title);
+           return title;
+        });
+        console.log(title);
+    }
+
+}
