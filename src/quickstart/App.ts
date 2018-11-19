0722ba002618bf8a093e64dcd88879b1da279036
xiyuan_fengyu <xiyuan_fengyu@163.com>
2018-11-19 16:11:19 +0800

    更新 ppspider 版本 quickstart 例子中增加浏览器下载和保存信息的日志打印

--- src/quickstart/App.ts
+++ src/quickstart/App.ts
@@ -2,8 +2,11 @@ import {Launcher, logger, PuppeteerWorkerFactory} from "ppspider";
 import {TestTask} from "./tasks/TestTask";
 import {config} from "./config";
 
-// import * as puppeteer from "puppeteer";
-// logger.debug(puppeteer.executablePath());
+// 打印 当前puppeteer版本对应的chromium的下载地址和本地保存目录
+import * as puppeteer from "puppeteer";
+const chromiumInfo = (puppeteer as any).createBrowserFetcher({})
+    .revisionInfo(require("puppeteer/package.json").puppeteer.chromium_revision);
+logger.debug("", "download url: " + chromiumInfo.url, "chromium path: " + chromiumInfo.executablePath.replace(/\\/g, '/'));
 
 @Launcher({
     workplace: __dirname + "/workplace",
