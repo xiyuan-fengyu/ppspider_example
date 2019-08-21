6c43f85a442393d0ae1cc36c5329d7d20a6f39a6
xiyuan_fengyu <xiyuan_fengyu@163.com>
2019-08-21 22:35:11 +0800

    更新ppspider版本 增加多PuppeteerWorkerFactory的例子

---
+++ src/examples/MultiChromeApp.ts
@@ -0,0 +1,53 @@
+import {DbHelperUi, Job, Launcher, logger, OnStart, PuppeteerWorkerFactory, Transient} from "ppspider";
+
+class TestTask {
+
+    @Transient()
+    private puppeteerWorkerFactories = [
+        new PuppeteerWorkerFactory({
+            headless: false,
+            devtools: true,
+            args: [
+                '--proxy-server=127.0.0.1:2007'
+            ]
+        }),
+        new PuppeteerWorkerFactory({
+            headless: false,
+            devtools: true,
+            args: [
+                '--proxy-server=127.0.0.1:3000'
+            ]
+        })
+    ];
+
+    @OnStart({
+        urls: ["0", "1"],
+        parallel: 2,
+        exeInterval: 0
+    })
+    async index(job: Job) {
+        const page = await this.puppeteerWorkerFactories[+job.url].get();
+        try {
+            await (async () => {
+                await page.goto("https://www.google.com");
+                logger.debug(await page.evaluate(() => document.title));
+            })();
+        }
+        finally {
+            await page.close();
+        }
+    }
+
+}
+
+@Launcher({
+    workplace: "workplace_quotes",
+    tasks: [
+        TestTask
+    ],
+    dataUis: [
+        DbHelperUi
+    ],
+    workerFactorys: []
+})
+class App {}
