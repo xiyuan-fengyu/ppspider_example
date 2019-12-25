0e9033a3e418b0546b7f0110bcf3379f4383eb2e
xiyuan_fengyu <xiyuan_fengyu@163.com>
2019-12-25 10:31:34 +0800

    更新版本，修改maxTry相关例子

--- src/examples/SetMaxTryApp.ts
+++ src/examples/SetMaxTryApp.ts
@@ -2,9 +2,12 @@ import {AddToQueue, FromQueue, Job, JobOverride, Launcher, OnStart} from "ppspid
 
 class TestTask {
 
+    // 多种方法设置 maxTry，取最大值
+
     // 方法一，在 JobOverride 回调函数中修改 job,datas._.maxTry
     @JobOverride("OnStart_TestTask_root")
     overrideMaxTry(job: Job) {
+        // 这里可以有条件地修改某些任务的最大尝试次数
         (job.datas._ || (job.datas._ = {} as any)).maxTry = 10;
         // 等价于
         // if (!job.datas._) {
@@ -13,15 +16,20 @@ class TestTask {
         // job.datas._.maxTry = 10;
     }
 
-    @OnStart({urls: "root_job"})
+    @OnStart({urls: "root_job", maxTry: 5}) // 方法三，统一设置该类任务的最大尝试次数
     @AddToQueue({name: "sub_jobs"})
     async root(job: Job) {
         console.log(job.queue + " " + job.datas._.maxTry);
 
         // 方法二，用子任务的url构建job，并设置job的maxTry
-        const subJob = new Job("sub_job");
-        subJob.datas._ = {maxTry: 10};
-        return subJob;
+        return new Job({
+            url: "sub_job",
+            datas: {
+                _: {
+                    maxTry: 10
+                }
+            }
+        });
     }
 
     @FromQueue({name: "sub_jobs"})
